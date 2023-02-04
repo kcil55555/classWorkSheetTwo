e5a68ebae5a994297be7f028bd02637e2766b769
RILEY <rhanstock@icloud.com>
2023-02-04 12:54:06 -0500

    die

--- src/funny.java
+++ src/funny.java
@@ -1,1 +1,1 @@
-import java.util.Scanner; public class funny { public static void main(String[] args) {}} //ugygyjfyjdieee
+import java.util.Scanner; public class funny { public static void main(String[] args)  {System.out.println("URTRASH");}} 
